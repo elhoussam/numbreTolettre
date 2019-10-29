d73b3a6791922aacf4b4c881b8a3f0932bd91104
elhoussam <houssam.glsd@gmail.com>
2019-10-29 10:39:04 +0100

    Generate new lib for the new feature

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 
-import static me.elhoussam.nbtolettre.NumToLet.print;
+import static me.elhoussam.mvn.generator .NumToLet.print;
 
-import me.elhoussam.nbtolettre.GenAr;
-import me.elhoussam.nbtolettre.GenEn;
-import me.elhoussam.nbtolettre.GenFr;
-import me.elhoussam.nbtolettre.NumToLet;
+import me.elhoussam.mvn.generator.GenAr;
+import me.elhoussam.mvn.generator.GenEn;
+import me.elhoussam.mvn.generator.GenFr;
+import me.elhoussam.mvn.generator.GenInd;
+import me.elhoussam.mvn.generator.NumToLet;
 
 public class Main {
 	public static void main(String argv[]) throws Exception{
@@ -12,13 +13,15 @@ public class Main {
 		long a = 1 ;
 		boolean bool = false ;
 		while ( true ) {
+
+			
 		String aa = "";
 		a = NumToLet.ScanInput();
-		//obj= new GenEn(); 	print("En : ");  aa =obj.Generate( a ); print("\n");
-		//obj= new GenFr(); 	print("FR : ");	aa =obj.Generate( a ); print("\n");
-		obj= new GenAr(); 	print("Ar : ");	aa = obj.Generate( a ); print("\n");
+		//obj= new GenEn();    obj.ToggleColor();	print("En : ");  aa =obj.Generate( a ); print("\n");
+		//obj= new GenFr();   obj.ToggleColor();	print("FR : ");	aa =obj.Generate( a ); print("\n"); 
+		//obj= new GenAr();  obj.ToggleColor();	print("Ar : ");	aa = obj.Generate( a ); print("\n");
+		obj= new GenInd(); 	print("Ind : ");	aa = obj.Generate( a ); print("\n");
 		aa = aa.trim() ;
-		obj.ToggleColor();
 		
 		}
 		
